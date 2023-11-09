29b9c4b14157ca9b031c42a22188082e387735dc
Marta Błażkowska <martablazkowska@MacBook-Pro-Marta.local>
2023-11-09 17:46:29 +0100

    fetching data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+
+  constructor(private heroService: HeroService) {}
+
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 }
