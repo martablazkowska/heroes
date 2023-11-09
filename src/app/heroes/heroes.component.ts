1182a3031aa90f85ba3efa22a054df92bd600073
Marta Błażkowska <martablazkowska@MacBook-Pro-Marta.local>
2023-11-09 18:18:01 +0100

    adds messages

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,7 +11,7 @@ import { HeroService } from '../hero.service';
 
 export class HeroesComponent {
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   heroes: Hero[] = [];
   selectedHero?: Hero;
@@ -26,6 +27,7 @@ export class HeroesComponent {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
 }
